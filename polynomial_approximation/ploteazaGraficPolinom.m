884b0f8af8200f3a57c10a4f4702975bb601958b
Stan Fedora Bianca <fedora-bianca.stan@my.fmi.unibuc.ro>
2016-03-23 17:29:16 +0200

    Update ploteazaGraficPolinom.m

--- polynomial_approximation/ploteazaGraficPolinom.m
+++ polynomial_approximation/ploteazaGraficPolinom.m
@@ -1,7 +1,7 @@
 function ploteazaGraficPolinom( p )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-x=linspace(0,2*pi); %returneaza un vector de 100 de elemente egal depatate ce acopera intervalul[0,1]
+x=linspace(0,2*pi); %returneaza un vector de 100 de elemente egal depatate ce acopera intervalul[0,2pi]
 y=polyval(p,x);
 plot(x,y,'r');
 
